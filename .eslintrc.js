ee469a4a7387a080a5c347863d866911d96878fd
crosstyan <cf595833532@gmail.com>
2021-08-12 06:21:19 +0800

    use submodule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,3 @@
-/* Crosstyan Version ESLint based on 
-   https://github.com/airbnb/javascript/tree/master/packages/eslint-config-airbnb-base  
-
-   - no semi. (I hate semi)
-   - @typescript-eslint
-   - no import (sometime this brings bugs) especially in Vue
-   - no node (not everyone running node)
-   - less strict but still warn you. 
-   - just my personal taste
-
-   Last one in extends properties has highest priority
-   I'm using TypeScript so @typescript-eslint is required
-*/
-
 module.exports = {
   "env": {
     "browser": true,
@@ -19,7 +5,7 @@ module.exports = {
     "node": true,
   },
   "extends": [
-    "./eslint/crosstyan.eslint",
+    "./eslintrc/crosstyan.eslint",
   ],
   "parser": "@typescript-eslint/parser",
   "parserOptions": {
